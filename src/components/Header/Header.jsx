9e9b6a2bb7185f1f363e9f956ddc0fb6369501b9
Rakesh Biswas <rbiswas01999@gmail.com>
2024-03-28 20:36:38 +0600

    redirects file added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
     // </div>
 
     return (
-        <div className="navbar bg-base-100 md:mx-10">
+        <div className="navbar bg-base-100 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
